0179e6a58d19e2701481bf2f710a3d95bff96338
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-11-16 05:14:36 -0400

    complete image filter route and form

--- app.py
+++ app.py
@@ -148,31 +148,26 @@ def image_filter():
 
     if request.method == 'POST':
         
-        # TODO: Get the user's chosen filter type (whichever one they chose in the form) and save
-        # as a variable
-        filter_type = ''
+        filter_type = request.form.get('filter_type')
         
-        # Get the image file submitted by the user
         image = request.files.get('users_image')
 
-        # TODO: call `save_image()` on the image & the user's chosen filter type, save the returned
-        # value as the new file path
+        new_file_path = save_image(image, filter_type)
 
-        # TODO: Call `apply_filter()` on the file path & filter type
+        apply_filter(new_file_path, filter_type)
 
         image_url = f'/static/images/{image.filename}'
 
         context = {
-            # TODO: Add context variables here for:
-            # - The full list of filter types
-            # - The image URL
+            'filter_types_keys': filter_types_dict.keys(),
+            'image_url': image_url
         }
 
         return render_template('image_filter.html', **context)
 
     else: # if it's a GET request
         context = {
-            # TODO: Add context variable here for the full list of filter types
+            'filter_types_keys': filter_types_dict.keys(),
         }
         return render_template('image_filter.html', **context)
 
